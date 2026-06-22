9688991400e4f7ad9b5f7a830d792edf82447d1e
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-06-22 21:26:11 +0200

    responsive Design added to skill, hero, about component

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-button',
-  imports: [NgTemplateOutlet],
+  imports: [NgTemplateOutlet,RouterLink],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
@@ -12,5 +13,7 @@ export class Button {
   disabled = input(false);
   href = input<string>();
   target = input<string>();
+  routerLink = input<string>(); 
+  fragment = input<string>();
   size = input<'normal' | 'large'>('normal');
 }
